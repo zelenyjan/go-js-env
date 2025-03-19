9388fedcd5a58f8c42b949e3090a4c358c6bafdf
Jan Zeleny <zelenja8@gmail.com>
2025-03-19 19:08:05 +0100

    Add README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Go JS ENV
+
+Simplified version of [react-inject-env](https://github.com/codegowhere/react-inject-env)
--- main.go
+++ main.go
@@ -15,9 +15,6 @@ const TemplateText = `window.envGo = {
 {{- end }}
 }`
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-
 func getEnvFromENV() map[string]string {
 	// store items to map
 	envs := make(map[string]string)
@@ -88,8 +85,6 @@ func writeToFile(envs map[string]string) {
 }
 
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
 	dataFromEnv := getEnvFromENV()
 	dataFromFile := getEnvFromFile()
 
